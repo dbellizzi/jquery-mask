26cbab3446d717e6733295fcbb61063ea2b4e74b
Dominick Bellizzi <dmb27@cornell.edu>
2012-06-27 23:45:43 -0700

    Update demo code

--- jquery-mask.js
+++ jquery-mask.js
@@ -17,6 +17,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
+console.log('here');
 (function($) {
 
   // NOTE: dimension objects are assumed to be immutable
